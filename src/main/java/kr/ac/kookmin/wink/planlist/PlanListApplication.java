54152b1262292ee57404e208e679a0d18e2520e4
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2024-07-23 12:56:45 +0900

    rename: 패키지 및 메인 클래스 이름 변경
    - 패키지 이름에 사용된 도메인은 임시로 wink 공식 홈페이지로 작성

--- src/main/java/kr/ac/kookmin/wink/planlist/PlanListApplication.java
+++ src/main/java/kr/ac/kookmin/wink/planlist/PlanListApplication.java
@@ -1,14 +1,14 @@
-package com.backend;
+package kr.ac.kookmin.wink.planlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class BackEndApplication {
+public class PlanListApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackEndApplication.class, args);
+        SpringApplication.run(PlanListApplication.class, args);
     }
 
 }
--- src/test/java/kr/ac/kookmin/wink/planlist/PlanListApplicationTests.java
+++ src/test/java/kr/ac/kookmin/wink/planlist/PlanListApplicationTests.java
@@ -1,10 +1,10 @@
-package com.backend;
+package kr.ac.kookmin.wink.planlist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackEndApplicationTests {
+class PlanListApplicationTests {
 
     @Test
     void contextLoads() {
