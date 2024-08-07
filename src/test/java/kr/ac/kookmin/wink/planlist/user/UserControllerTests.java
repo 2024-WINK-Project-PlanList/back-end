aab8a4031bc9468d50cf4bd170d2a44ff0e2ba34
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2024-08-08 01:15:29 +0900

    test: 임시 계정으로 JWT 토큰 생성 테스트

--- src/test/java/kr/ac/kookmin/wink/planlist/user/UserControllerTests.java
+++ src/test/java/kr/ac/kookmin/wink/planlist/user/UserControllerTests.java
@@ -1,5 +1,6 @@
 package kr.ac.kookmin.wink.planlist.user;
 
+import jakarta.transaction.Transactional;
 import kr.ac.kookmin.wink.planlist.user.service.UserService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -11,10 +12,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class UserControllerTests {
     @Autowired
     protected MockMvc mockMvc;
@@ -35,5 +39,20 @@ public class UserControllerTests {
         result.andExpect(status().is2xxSuccessful());
 
         Assertions.assertThat(result.andReturn().getResponse().getContentAsString()).isEqualTo("false");
+    }
+
+    @DisplayName("temp: 임시 계정 생성/조회")
+    @Test
+    public void tempAccountTest() throws Exception {
+        //given
+        String url = "/auth/temp";
+
+        //when
+        ResultActions result = mockMvc.perform(post(url));
+
+        //then
+        result
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.accessToken").isNotEmpty());
     }
 }
