c90d441ed259aa25b103ddef868c4f21eab4b652
clzlol <nyc0527@naver.com>
2024-09-14 17:53:38 +0900

    feat: 공유 스케쥴 연관관계 매핑

--- src/main/java/kr/ac/kookmin/wink/planlist/shared/schedule/domain/UserSharedSchedule.java
+++ src/main/java/kr/ac/kookmin/wink/planlist/shared/schedule/domain/UserSharedSchedule.java
@@ -1,4 +1,48 @@
 package kr.ac.kookmin.wink.planlist.shared.schedule.domain;
 
-public class UserSharedSchedule {
+import jakarta.persistence.*;
+import kr.ac.kookmin.wink.planlist.individual.schedule.domain.ScheduleOpenStatus;
+import kr.ac.kookmin.wink.planlist.shared.calendar.domain.SharedCalendar;
+import kr.ac.kookmin.wink.planlist.user.domain.User;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+
+import java.time.LocalDate;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "\"userSharedSchedule\"")
+@Entity
+public class UserSharedSchedule implements Persistable<UserSharedScheduleId> {
+
+    @EmbeddedId
+    private UserSharedScheduleId id;
+
+    @MapsId("userId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @MapsId("sharedScheduleId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private SharedSchedule sharedSchedule;
+
+    private String content;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private ScheduleOpenStatus openStatus;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @Override
+    public boolean isNew() {
+        return createdAt == null;
+    }
 }
