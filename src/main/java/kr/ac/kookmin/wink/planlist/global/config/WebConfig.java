c11bad5d0c65c0b21e790ff8a7cf0bbad372e2a2
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2024-09-15 03:56:54 +0900

    fix: patch 요청 시, cors 현상 수정

--- src/main/java/kr/ac/kookmin/wink/planlist/global/config/WebConfig.java
+++ src/main/java/kr/ac/kookmin/wink/planlist/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
                 .allowCredentials(true)
