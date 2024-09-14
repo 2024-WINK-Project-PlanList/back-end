88cc770976dd6c903a3a82d0a89bff99aa133bfa
Minu <46252059+alsdn4956@users.noreply.github.com>
2024-09-15 01:37:34 +0900

    feat: 배포 url 추가

--- src/main/java/kr/ac/kookmin/wink/planlist/global/config/WebConfig.java
+++ src/main/java/kr/ac/kookmin/wink/planlist/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://3.38.182.52:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
